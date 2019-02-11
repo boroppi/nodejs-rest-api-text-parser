6e6aaa8e93e2f0bfe8855e3fe90b69e3e7adacb9
Burak Kaya <200383524@student.georgianc.on.ca>
2019-02-11 16:06:26 -0500

    test

--- index.js
+++ index.js
@@ -1,104 +1,15 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+var http = require('http');
 
-const app = express();
+var server = http.createServer(function (request, response) {
 
-app.use(bodyParser.text({
-  type: "text/html"
-}));
+  response.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+  response.end("Hello World!");
 
-app.get("/", (req, res) => {
-  console.log("Responding to root route");
-  res.send(`<h1>Hello from ROOT!</h1>`);
 });
 
-// parse email
-app.post("/api/parse_email", (req, res) => {
-  let object = parseEmail(req.body);
-  let script = generateInsertScript(object)
-  res.send(script);
-});
+var port = process.env.PORT || 1337;
+server.listen(port);
 
-// localhost:3003
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is up and listening on port: ${port}...`);
-});
-
-function parseEmail(email) {
-  // Initialize empty object to be filled later
-  let json = {};
-
-  // Workorder
-  const workorder_expr = /Work Order.([^\.]*)/;
-  let workorder = workorder_expr.exec(email)[1];
-  json.workorder = workorder;
-
-  // Approved by
-  const approved_by_name_expr = /Approved by:[\n\r].*Name:\s*([^\n\r]*)/;
-  const approved_by_email_expr = /Approved by:[\n\r].*[\n\r]*Email:\s*([^\n\r]*)/;
-  let approved_by_name = approved_by_name_expr.exec(email)[1];
-  let approved_by_email = approved_by_email_expr.exec(email)[1];
-  json.approvedBy = {};
-  json.approvedBy.approvedByName = approved_by_name;
-  json.approvedBy.approvedByEmail = approved_by_email;
-
-  // Requester
-  const requester_name_expr = /Requester:[\n\r].*Name:\s*([^\n\r]*)/;
-  const requester_email_expr = /Requester:[\n\r].*[\n\r]*Email:\s*([^\n\r]*)/;
-  json.requester = {};
-  json.requester.requesterName = requester_name_expr.exec(email)[1];
-  json.requester.requesterEmail = requester_email_expr.exec(email)[1];
-
-  // Item requested
-  const item_requested_expr = /\s*Item requested:\s*([^\n\r]*)/;
-  json.itemRequested = item_requested_expr.exec(email)[1];
-
-  // Request type
-  const request_type_expr = /\s*Request type:\s*([^\n\r]*)/;
-  json.requestType = request_type_expr.exec(email)[1];
-
-  // Request details
-  const cluster_name_expr = /\s*Cluster Name:\s*([^\n\r]*)/;
-  const role_expr = /\s*Role:\s*([^\n\r]*)/;
-  //const servers_expr = /\s*Server\(s\) to be added:\s*([^\n\r]*)/
-  const servers_expr = /\s*Server\(s\) to be added:[\s\w,]*(?=\nUser)/;
-  const users_expr = /\s*User\(s\) to be added:\s*([^\n\r]*)/;
-  json.requestDetails = {};
-  json.requestDetails.clusterName = cluster_name_expr.exec(email)[1];
-  json.requestDetails.role = role_expr.exec(email)[1];
-  let servers = servers_expr.exec(email)[0];
-  servers = servers.split(/\s*Server\(s\) to be added:\s*/)[1];
-  servers = servers.split(/\s*,\s*/);
-  console.log(servers);
-  //servers = servers.split(/\s*Server\(s\) to be added:\s*/);
-  json.requestDetails.servers = servers;
-  let users = users_expr.exec(email)[1];
-  users = users.split(/(?<=\))\,\s/);
-  json.requestDetails.users = users;
-
-  //Payment details
-  const cost_centre = /\s*Cost Centre:\s*([^\n\r]*)/
-  json.paymentDetails = {};
-  json.paymentDetails.costCentre = cost_centre.exec(email)[1];
-
-  return json;
-}
-
-
-function generateInsertScript(object) {
-
-  var script = "";
-
-  let {
-    role
-  } = object.requestDetails;
-
-  for (var server of object.requestDetails.servers) {
-    for (var user of object.requestDetails.users) {
-      script += `INSERT INTO [dbo].[Process] ([server],[user_name],[role],[action]) VALUES ('${server}','${user}','${role}','ADD');\n`
-    }
-  }
-
-  return script;
-}
+console.log("Server running at http://localhost:%d", port);
